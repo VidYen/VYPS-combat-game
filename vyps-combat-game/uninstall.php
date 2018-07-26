20ccda4c9011e6a02e84a55c2e6bf26d6f4b1fd8
Felty <felty@vidyen.com>
2018-07-26 15:16:15 -0400

    uinstall update to fix headers issue

--- vyps-combat-game/uninstall.php
+++ vyps-combat-game/uninstall.php
@@ -17,13 +17,12 @@ global $wpdb;
 
 $table_name_pending_battles = $wpdb->prefix . 'vypsg_pending_battles';
 $table_name_battles = $wpdb->prefix . 'vypsg_battles';
-//$table_name_tracking = $wpdb->prefix . 'vypsg_tracking'; //I don't think this actually needs to be dropped, but it seems to have more meta than it should. It's violating my log vision though.
+$table_name_tracking = $wpdb->prefix . 'vypsg_tracking'; //I don't think this actually needs to be dropped, but it seems to have more meta than it should. It's violating my log vision though.
 
 // drop the table from the database. NOTE: I called each without a loop as this should be very simple and specific. -Felty
 $wpdb->query( "DROP TABLE IF EXISTS $table_name_pending_battles" );
 $wpdb->query( "DROP TABLE IF EXISTS $table_name_battles" );
+$wpdb->query( "DROP TABLE IF EXISTS $table_name_tracking" );
 
 // drop the table from the database. NOTE: Again I called each without a loop as this should be very simple and specific.
 // Also I feel like I should just call the variable what they are so can be visually followed rather than traced.  -Felty
-
-//$wpdb->query( "DROP TABLE IF EXISTS $table_name_tracking" );
